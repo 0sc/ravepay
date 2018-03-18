e4b3c278c5cd07229a24c3da5253955fad20b0fb
Osmond Oscar <osmond.oranagwa@andela.com>
2018-03-18 12:11:56 +0100

    add charge encryption implementation

---
+++ encryption.go
@@ -0,0 +1,50 @@
+package rave
+
+import (
+	"bytes"
+	"crypto/des"
+	"crypto/md5"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+)
+
+func getEncryptionKey(seckey string) string {
+	h := md5.New()
+	io.WriteString(h, seckey)
+	keyMD5 := fmt.Sprintf("%x", h.Sum(nil))
+	keyMD5Last12 := keyMD5[len(keyMD5)-12:]
+
+	adjustedSeckey := strings.Replace(seckey, "FLWSECK-", "", 1)
+	adjustedSeckeyFirst12 := adjustedSeckey[:12]
+
+	return adjustedSeckeyFirst12 + keyMD5Last12
+}
+
+// https://github.com/golang/go/issues/5597
+func tripleDESEncrypt(payload, key []byte) string {
+	block, err := des.NewTripleDESCipher(key)
+	if err != nil {
+		log.Println("couldn't create 3DESC cipher: ", err)
+		return ""
+	}
+
+	bs := block.BlockSize()
+	if numStrandedBytes := len(payload) % bs; numStrandedBytes != 0 {
+		paddingAmt := bs - numStrandedBytes
+		padding := bytes.Repeat([]byte{byte(paddingAmt)}, paddingAmt)
+		payload = append(payload, padding...)
+	}
+
+	cipher := make([]byte, len(payload))
+	cipherDup := cipher
+	for len(payload) > 0 {
+		block.Encrypt(cipher, payload)
+		payload = payload[bs:]
+		cipher = cipher[bs:]
+	}
+
+	return base64.StdEncoding.EncodeToString(cipherDup)
+}
---
+++ encryption_test.go
@@ -0,0 +1,70 @@
+package rave
+
+import (
+	"testing"
+)
+
+func Test_getEncryptionKey(t *testing.T) {
+	type args struct {
+		seckey string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "returns the expected encryption key - 1",
+			args: args{"FLWSECK-bb971402072265fb156e90a3578fe5e6-X"},
+			want: "bb9714020722eb4cf7a169f2",
+		},
+		{
+			name: "returns the expected encryption key - 2",
+			args: args{"FLWSECK-6b32914d4d60c10d0ef72bdad734134a-X"},
+			want: "6b32914d4d60cb85d8eb73db",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getEncryptionKey(tt.args.seckey); got != tt.want {
+				t.Errorf("getKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_tripleDESEncrypt(t *testing.T) {
+	type args struct {
+		payload []byte
+		key     []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "returns the expected 3DES encrypted payload - 1",
+			args: args{
+				payload: []byte("A 16 byte string"),
+				key:     []byte("6b32914d4d60cb85d8eb73db"),
+			},
+			want: "9fx+9uGjG+Oikq8syKpfeg==",
+		},
+		{
+			name: "returns the expected 3DES encrypted payload - 2",
+			args: args{
+				payload: []byte("Hello world"),
+				key:     []byte("bb9714020722eb4cf7a169f2"),
+			},
+			want: "Lgk7z/IvTT9mx3t9vOzHmg==",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tripleDESEncrypt(tt.args.payload, tt.args.key); got != tt.want {
+				t.Errorf("tripleDESEncrypt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
