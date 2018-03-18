efcdf8ed4d1741133d1a89ff97f6d61ae2d85689
Osmond Oscar <osmond.oranagwa@andela.com>
2018-03-18 23:17:02 +0100

    add charge bank account implementation

--- account.go
+++ account.go
@@ -1,16 +1,66 @@
 package rave
 
+import (
+	"encoding/json"
+	"log"
+)
+
+// Account is a type that encapsulates rave's account description
+// It has all account attributes necessary for rave api card references
+// It also implements the chargable interface required for making charge requests and validating them
 type Account struct {
-	AccountBank          string `json:"account_bank"`
+	AccountBank          string `json:"accountbank"`
 	AccountIsBlacklisted int    `json:"account_is_blacklisted"`
-	AccountNumber        string `json:"account_number"`
+	AccountNumber        string `json:"accountnumber"`
 	AccountToken         struct {
 		Token string `json:"token"`
 	} `json:"account_token"`
-	CreatedAt string      `json:"createdAt"`
-	DeletedAt interface{} `json:"deletedAt"`
-	FirstName string      `json:"first_name"`
-	ID        int         `json:"id"`
-	LastName  string      `json:"last_name"`
-	UpdatedAt string      `json:"updatedAt"`
+	ChargeAccountURL         string      `json:"-"`
+	ValidateAccountChargeURL string      `json:"-"`
+	Country                  string      `json:"country"`
+	CreatedAt                string      `json:"createdAt"`
+	Currency                 string      `json:"currency"`
+	DeletedAt                interface{} `json:"deletedAt"`
+	FirstName                string      `json:"first_name"`
+	ID                       int         `json:"id"`
+	LastName                 string      `json:"last_name"`
+	Passcode                 string      `json:"passcode"`
+	UpdatedAt                string      `json:"updatedAt"`
+}
+
+// ChargeURL is an implemenation of the Chargeable interface
+// it returns the url to be used for charging the given account
+func (a *Account) ChargeURL() string {
+	if a.ChargeAccountURL == "" {
+		a.ChargeAccountURL = ChargeAccountURL
+	}
+	return a.ChargeAccountURL
+}
+
+// ValidateChargeURL is an implemenation of the Chargeable interface
+// it returns the url to be used for charging the given account
+func (a *Account) ValidateChargeURL() string {
+	if a.ValidateAccountChargeURL == "" {
+		a.ValidateAccountChargeURL = ValidateAccountChargeURL
+	}
+	return a.ValidateAccountChargeURL
+}
+
+// BuildChargeRequestPayload is an implemenation of the Chargeable interface
+// it returns the byte representation of the charge request client
+// at the ChargeRequest level, chargeables are merely interface objects
+// so trying to compose a struct with an interface object results in go adding the interface name key to the result bytes
+// see https://play.golang.com/p/MFfbuPLrjo6
+// so here we upend it so the individual concrete types do the marshalling
+func (a *Account) BuildChargeRequestPayload(creq *ChargeRequest) []byte {
+	creq.PaymentType = "account"
+	payload := struct {
+		*Account
+		*ChargeRequest
+	}{a, creq}
+	b, err := json.Marshal(payload)
+	if err != nil {
+		log.Println("couldn't marshal payload: ", err)
+	}
+	return b
 }
---
+++ account_test.go
@@ -0,0 +1,73 @@
+package rave
+
+import "testing"
+
+func TestAccount_ChargeURL(t *testing.T) {
+	type fields struct {
+		ChargeAccountURL string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name:   "returns the ChargeAccountURL in the account object if present",
+			fields: fields{"https://charge.account.url"},
+			want:   "https://charge.account.url",
+		},
+		{
+			name: "set's the object ChargeAccountURL to config's ChargeAccountURL and returns it",
+			want: ChargeAccountURL,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Account{
+				ChargeAccountURL: tt.fields.ChargeAccountURL,
+			}
+			if got := a.ChargeURL(); got != tt.want {
+				t.Errorf("Account.ChargeURL() = %v, want %v", got, tt.want)
+			}
+
+			if a.ChargeAccountURL != tt.want {
+				t.Errorf("Account.ChargeURL() = %v, want %v", a.ChargeAccountURL, tt.want)
+			}
+		})
+	}
+}
+
+func TestAccount_ValidateChargeURL(t *testing.T) {
+	type fields struct {
+		ValidateAccountChargeURL string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name:   "returns ValidateAccountChargeURL in the account object if present",
+			fields: fields{"https://validate.account.charge.url"},
+			want:   "https://validate.account.charge.url",
+		},
+		{
+			name: "set's the object ValidateAccountChargeURL to the config ValidateAccountChargeURL and returns it",
+			want: ValidateAccountChargeURL,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Account{
+				ValidateAccountChargeURL: tt.fields.ValidateAccountChargeURL,
+			}
+			if got := a.ValidateChargeURL(); got != tt.want {
+				t.Errorf("Account.ValidateChargeURL() = %v, want %v", got, tt.want)
+			}
+
+			if a.ValidateAccountChargeURL != tt.want {
+				t.Errorf("Account.ValidateChargeURL() = %v, want %v", a.ValidateAccountChargeURL, tt.want)
+			}
+		})
+	}
+}
