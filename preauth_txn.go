f288c675d5ebf3511f8954e43f9dbd8feddc6d9c
Osmond Oscar <osmond.oranagwa@andela.com>
2018-03-20 01:51:02 +0100

    add implementation for pre authorized payment transactions

---
+++ preauth_txn.go
@@ -0,0 +1,77 @@
+package rave
+
+// FIXME: Done to enable testing
+var (
+	capturePreAuthURL      = CapturePreAuthPaymentURL
+	voidorRefundPreAuthURL = VoidorRefundPreAuthURL
+)
+
+// CapturePreAuthPayment makes request to rave's capture endpoint to claim preauth payments
+// It takes the flwRef as param and returns the capture response and any error that occures
+// https://flutterwavedevelopers.readme.io/v2.0/reference#capture
+func CapturePreAuthPayment(ref string) (*ChargeResponse, error) {
+	resp := &ChargeResponse{}
+	payload := struct {
+		SECKEY string `json:"SECKEY"`
+		FlwRef string `json:"flwRef"`
+	}{secKey, ref}
+
+	err := sendRequestAndParseResponse("POST", capturePreAuthURL, payload, resp)
+
+	return resp, err
+}
+
+// PreAuthResponse is a type of rave response for refund or void preauth payment response
+type PreAuthResponse struct {
+	Data struct {
+		Data struct {
+			AuthorizeID              string      `json:"authorizeId"`
+			Avsresponsecode          interface{} `json:"avsresponsecode"`
+			Avsresponsemessage       interface{} `json:"avsresponsemessage"`
+			Otptransactionidentifier interface{} `json:"otptransactionidentifier"`
+			Redirecturl              interface{} `json:"redirecturl"`
+			Responsecode             string      `json:"responsecode"`
+			Responsehtml             interface{} `json:"responsehtml"`
+			Responsemessage          string      `json:"responsemessage"`
+			Responsetoken            interface{} `json:"responsetoken"`
+			Transactionreference     string      `json:"transactionreference"`
+		} `json:"data"`
+		Status string `json:"status"`
+	} `json:"data"`
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
+
+// RefundPreAuthPayment fullfiles the raves preauth feature by refunding the preauthorized paymemnt
+// It takes the flwRef and makes a request to refund the txn
+// It returns the response and any error that occurs
+// https://flutterwavedevelopers.readme.io/v2.0/reference#refund-or-void
+func RefundPreAuthPayment(ref string) (*PreAuthResponse, error) {
+	resp := &PreAuthResponse{}
+	payload := struct {
+		Action string `json:"action"`
+		FlwRef string `json:"ref"`
+		SECKEY string `json:"SECKEY"`
+	}{"refund", secKey, ref}
+
+	err := sendRequestAndParseResponse("POST", voidorRefundPreAuthURL, payload, resp)
+
+	return resp, err
+}
+
+// VoidPreAuthPayment fullfiles the raves preauth feature by voiding the preauthorized paymemnt
+// It takes the flwRef and makes a request to void the txn
+// It returns the response and any error that occurs
+// https://flutterwavedevelopers.readme.io/v2.0/reference#refund-or-void
+func VoidPreAuthPayment(ref string) (*PreAuthResponse, error) {
+	resp := &PreAuthResponse{}
+	payload := struct {
+		Action string `json:"action"`
+		FlwRef string `json:"ref"`
+		SECKEY string `json:"SECKEY"`
+	}{"void", secKey, ref}
+
+	err := sendRequestAndParseResponse("POST", voidorRefundPreAuthURL, payload, resp)
+
+	return resp, err
+}
---
+++ preauth_txn_test.go
@@ -0,0 +1,208 @@
+package rave
+
+import (
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestCapturePreAuthPayment(t *testing.T) {
+	handler := &testServer{}
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	type args struct {
+		ref string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		respBody string
+		want     *ChargeResponse
+		wantErr  bool
+	}{
+		{
+			name:     "returns the capture response",
+			respBody: successfulPreAuthPaymentCaptureResponse,
+			args: args{
+				ref: "FLW-MOCK-839c1abc23b6a4bbb9da807d54c5bbda",
+			},
+			want: &ChargeResponse{
+				Status:  "success",
+				Message: "Capture complete",
+				Data: chargeResponseData{
+					AccountID:             48,
+					Amount:                20,
+					Appfee:                0.25,
+					AuthModelUsed:         "NOAUTH",
+					Authurl:               "N/A",
+					ChargedAmount:         20.25,
+					ChargeResponseMessage: "Approved",
+					ChargeResponseCode:    "00",
+					ChargeType:            "preauth",
+					CreatedAt:             "2017-10-26T03:56:42.000Z",
+					Currency:              "NGN",
+					CustomerID:            88067,
+					Cycle:                 "one-time",
+					DeviceFingerprint:     "69e6b7f0b72037aa8428b70fbe03986c",
+					FlwRef:                "FLW-MOCK-839c1abc23b6a4bbb9da807d54c5bbda",
+					FraudStatus:           "ok",
+					ID:                    194211,
+					IP:                    "::ffff:10.37.225.74",
+					Narration:             "FLW-PBF CARD Transaction ",
+					PaymentID:             "47433",
+					PaymentType:           "card",
+					RedirectURL:           "http://127.0.0",
+					Status:                "successful",
+					TxRef:                 "MC-1508990174050",
+					UpdatedAt:             "2017-10-26T04:20:21.000Z",
+					Vbvrespcode:           "00",
+					Vbvrespmessage:        "Approved",
+					Customer: Customer{
+						AccountID: 48,
+						CreatedAt: "2017-10-26T03:39:45.000Z",
+						Email:     "tester@flutter.co",
+						FullName:  "temi desola",
+						ID:        88067,
+						Phone:     "08056552980",
+						UpdatedAt: "2017-10-26T03:39:45.000Z",
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			capturePreAuthURL = server.URL
+			handler.resp = []byte(tt.respBody)
+			secKey = "FLWSECK-c51891678d48c39eff3701ff686bdb69-X"
+
+			got, err := CapturePreAuthPayment(tt.args.ref)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CapturePreAuthPayment() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CapturePreAuthPayment() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRefundPreAuthPayment(t *testing.T) {
+	handler := &testServer{}
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	type args struct {
+		ref string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		respBody string
+		want     *PreAuthResponse
+		wantErr  bool
+	}{
+		{
+			name: "returns the refund preauth payment response if successful",
+			args: args{
+				ref: "FLW-MOCK-839c1abc23b6a4bbb9da807d54c5bbda",
+			},
+			respBody: successfulPreAuthPaymentRefundResponse,
+			want: &PreAuthResponse{
+				Message: "Refund or void complete",
+				Status:  "success",
+			},
+			wantErr: false,
+		},
+		{
+			name: "returns the refund preauth payment response if unsuccessful",
+			args: args{
+				ref: "FLW-MOCK-839c1abc23b6a4bbb9da807d54c5bbda",
+			},
+			respBody: failedPreAuthPaymentRefundResponse,
+			want: &PreAuthResponse{
+				Message: "No transaction found",
+				Status:  "error",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			voidorRefundPreAuthURL = server.URL
+			handler.resp = []byte(tt.respBody)
+			secKey = "FLWSECK-c51891678d48c39eff3701ff686bdb69-X"
+
+			got, err := RefundPreAuthPayment(tt.args.ref)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RefundPreAuthPayment() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if got.Message != tt.want.Message || got.Status != tt.want.Status {
+				t.Errorf("RefundPreAuthPayment() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func TestVoidPreAuthPayment(t *testing.T) {
+	handler := &testServer{}
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	type args struct {
+		ref string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		respBody string
+		want     *PreAuthResponse
+		wantErr  bool
+	}{
+		{
+			name: "returns the refund preauth payment response if successful",
+			args: args{
+				ref: "FLW-MOCK-839c1abc23b6a4bbb9da807d54c5bbda",
+			},
+			respBody: successfulPreAuthPaymentRefundResponse,
+			want: &PreAuthResponse{
+				Message: "Refund or void complete",
+				Status:  "success",
+			},
+			wantErr: false,
+		},
+		{
+			name: "returns the refund preauth payment response if unsuccessful",
+			args: args{
+				ref: "FLW-MOCK-839c1abc23b6a4bbb9da807d54c5bbda",
+			},
+			respBody: failedPreAuthPaymentRefundResponse,
+			want: &PreAuthResponse{
+				Message: "No transaction found",
+				Status:  "error",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			voidorRefundPreAuthURL = server.URL
+			handler.resp = []byte(tt.respBody)
+			secKey = "FLWSECK-c51891678d48c39eff3701ff686bdb69-X"
+
+			got, err := VoidPreAuthPayment(tt.args.ref)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("RefundPreAuthPayment() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if got.Message != tt.want.Message || got.Status != tt.want.Status {
+				t.Errorf("RefundPreAuthPayment() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
