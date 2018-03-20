d0a2db9224e5fe4fc0acf6f8df29783e9340d570
Osmond Oscar <osmond.oranagwa@andela.com>
2018-03-20 01:55:00 +0100

    add implementation for fee endpoint

---
+++ fees.go
@@ -0,0 +1,41 @@
+package rave
+
+// FIXME: Done to enable testing
+var getFeeURL = GetFeeURL
+
+// GetFeeRequest encapsulates the params need for requesting fee amount from the rave api
+// https://flutterwavedevelopers.readme.io/v2.0/reference#get-fees
+type GetFeeRequest struct {
+	Amount    string `json:"amount"`
+	PBFPubKey string `json:"PBFPubKey"`
+	Currency  string `json:"currency"`
+	PType     string `json:"ptype,omitempty"`
+	Card6     string `json:"card6,omitempty"`
+}
+
+// GetFeeResponse is a type of rave's response to a get fee request
+type GetFeeResponse struct {
+	Data struct {
+		ChargeAmount string  `json:"charge_amount"`
+		Fee          float64 `json:"fee"`
+		Merchantfee  string  `json:"merchantfee"`
+		Ravefee      string  `json:"ravefee"`
+	} `json:"data"`
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
+
+// GetFee returns rave's fee response for the given get fee request
+// it returns any error that occures
+func GetFee(p *GetFeeRequest) (*GetFeeResponse, error) {
+	// TODO: add request params validation
+	if p.PBFPubKey == "" {
+		p.PBFPubKey = PBFPubKey
+	}
+
+	resp := &GetFeeResponse{}
+
+	err := sendRequestAndParseResponse("POST", getFeeURL, p, resp)
+	return resp, err
+
+}
---
+++ fees_test.go
@@ -0,0 +1,77 @@
+package rave
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetFee(t *testing.T) {
+	handler := &testServer{}
+	server := httptest.NewServer(handler)
+	defer server.Close()
+	handler.resp = []byte(getFeeResponse)
+	getFeeURL = server.URL
+
+	type args struct {
+		p *GetFeeRequest
+	}
+	tests := []struct {
+		name       string
+		args       args
+		want       *GetFeeResponse
+		wantPubkey string
+		wantErr    bool
+	}{
+		{
+			name: "returns the GetFee response",
+			args: args{
+				p: &GetFeeRequest{},
+			},
+			want: &GetFeeResponse{
+				Status:  "success",
+				Message: "Charged fee",
+			},
+			wantPubkey: PBFPubKey,
+			wantErr:    false,
+		},
+		{
+			name: "sets the PBFPubKey if empty",
+			args: args{
+				p: &GetFeeRequest{},
+			},
+			want: &GetFeeResponse{
+				Status:  "success",
+				Message: "Charged fee",
+			},
+			wantPubkey: PBFPubKey,
+			wantErr:    false,
+		},
+		{
+			name: "doesn't override the PBFPubKey if present",
+			args: args{
+				p: &GetFeeRequest{PBFPubKey: "my-pub-key"},
+			},
+			want: &GetFeeResponse{
+				Status:  "success",
+				Message: "Charged fee",
+			},
+			wantPubkey: "my-pub-key",
+			wantErr:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetFee(tt.args.p)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetFee() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if key := tt.args.p.PBFPubKey; key != tt.wantPubkey {
+				t.Errorf("PBFPubkey = %s, want %s", key, tt.wantPubkey)
+			}
+			if got.Message != tt.want.Message || got.Status != tt.want.Status {
+				t.Errorf("GetFee() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
