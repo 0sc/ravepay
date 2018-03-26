79c2ca5b23ac398508fcc44a4ef95afbb6601fcd
Osmond Oscar <osmond.oranagwa@andela.com>
2018-03-26 11:52:30 +0100

    update config implementation to enable switching mode

--- config.go
+++ config.go
@@ -1,22 +1,65 @@
 package rave
 
+import (
+	"fmt"
+)
+
 const (
 	// TransactionVerificationURL is Rave's verification URL
-	TransactionVerificationURL        = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/verify"
-	TransactionVerificationRequeryURL = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/xrequery"
-	ChargeCardURL                     = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/charge"
-	ChargeMpesaURL                    = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/charge"
-	ChargeUSSDURL                     = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/validatecharge"
-	ValidateCardChargeURL             = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/validatecharge"
-	ChargeAccountURL                  = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/charge"
-	MobileMoneyGHChargeURL            = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/charge"
-	ValidateAccountChargeURL          = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/validate"
-	ListBanksURL                      = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/flwpbf-banks.js?json=1"
-	SecretKey                         = "FLWSECK-bb971402072265fb156e90a3578fe5e6-X"
-	PBFPubKey                         = "FLWPUBK-e634d14d9ded04eaf05d5b63a0a06d2f-X"
-	CapturePreAuthPaymentURL          = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/capture"
-	VoidorRefundPreAuthURL            = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/refundorvoid"
-	GetFeeURL                         = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/fee"
-	RefundTxnURL                      = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/gpx/merchant/transactions/refund"
-	ForexURL                          = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com/flwv3-pug/getpaidx/api/forex"
+	TransactionVerificationURL        = "/flwv3-pug/getpaidx/api/verify"
+	TransactionVerificationRequeryURL = "/flwv3-pug/getpaidx/api/xrequery"
+
+	defaultChargeURL       = "/flwv3-pug/getpaidx/api/charge"
+	ChargeCardURL          = "/flwv3-pug/getpaidx/api/charge"
+	ChargeMpesaURL         = "/flwv3-pug/getpaidx/api/charge"
+	ChargeAccountURL       = "/flwv3-pug/getpaidx/api/charge"
+	MobileMoneyGHChargeURL = "/flwv3-pug/getpaidx/api/charge"
+
+	ChargeUSSDURL            = "/flwv3-pug/getpaidx/api/validatecharge"
+	ValidateCardChargeURL    = "/flwv3-pug/getpaidx/api/validatecharge"
+	ValidateAccountChargeURL = "/flwv3-pug/getpaidx/api/validate"
+	ListBanksURL             = "/flwv3-pug/getpaidx/api/flwpbf-banks.js?json=1"
+	CapturePreAuthPaymentURL = "/flwv3-pug/getpaidx/api/capture"
+	VoidorRefundPreAuthURL   = "/flwv3-pug/getpaidx/api/refundorvoid"
+	GetFeeURL                = "/flwv3-pug/getpaidx/api/fee"
+	RefundTxnURL             = "/gpx/merchant/transactions/refund"
+	ForexURL                 = "/flwv3-pug/getpaidx/api/forex"
+
+	testModeBaseURL = "http://flw-pms-dev.eu-west-1.elasticbeanstalk.com"
+	liveModeBaseURL = "https://api.ravepay.co"
 )
+
+var (
+	currentMode = "test"
+	baseURL     = testModeBaseURL
+
+	// PBFPubKey is your rave secret key
+	PBFPubKey = "FLWPUBK-e634d14d9ded04eaf05d5b63a0a06d2f-X"
+	// SecretKey is your rave secret key
+	SecretKey = "FLWSECK-bb971402072265fb156e90a3578fe5e6-X"
+)
+
+// CurrentMode returns the current mode of operation, live or test
+// This actual variable itself, currentMode is not exposed to prevent direct (external) modification
+// (external) modification should only be done via the helper methods below
+func CurrentMode() string {
+	return currentMode
+}
+
+// SwitchToLiveMode changes to current operation mode to live
+// Rave api requests in the live mode are made to the real live rave api servers and not the test servers
+func SwitchToLiveMode() {
+	currentMode = "live"
+	baseURL = liveModeBaseURL
+}
+
+// SwitchToTestMode changes to current operation mode to test
+// Rave api requests in the live mode are made to the test rave api servers and not the live servers
+func SwitchToTestMode() {
+	currentMode = "test"
+	baseURL = testModeBaseURL
+}
+
+func buildURL(path string) string {
+	return fmt.Sprintf("%s%s", baseURL, path)
+}
---
+++ config_test.go
@@ -0,0 +1,116 @@
+package rave
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSwitchToLiveMode(t *testing.T) {
+	currentMode = ""
+	baseURL = ""
+
+	SwitchToLiveMode()
+
+	if currentMode != "live" {
+		t.Errorf("want %s got %s", "live", currentMode)
+	}
+
+	if baseURL != liveModeBaseURL {
+		t.Errorf("want %s got %s", liveModeBaseURL, baseURL)
+	}
+}
+
+func TestSwitchToTestMode(t *testing.T) {
+	currentMode = ""
+	baseURL = ""
+
+	SwitchToTestMode()
+
+	if currentMode != "test" {
+		t.Errorf("want %s got %s", "test", currentMode)
+	}
+
+	if baseURL != testModeBaseURL {
+		t.Errorf("want %s got %s", testModeBaseURL, baseURL)
+	}
+}
+
+func Test_buildURL(t *testing.T) {
+	type args struct {
+		path string
+		mode string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "builds url with the live mode base url",
+			args: args{
+				path: "to/rave/endpoint",
+				mode: "live",
+			},
+			want: fmt.Sprintf("%s%s", liveModeBaseURL, "to/rave/endpoint"),
+		},
+		{
+			name: "builds url with the test mode base url",
+			args: args{
+				path: "to/rave/endpoint",
+				mode: "test",
+			},
+			want: fmt.Sprintf("%s%s", testModeBaseURL, "to/rave/endpoint"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			switch tt.args.mode {
+			case "live":
+				SwitchToLiveMode()
+			case "test":
+				SwitchToTestMode()
+			}
+
+			if got := buildURL(tt.args.path); got != tt.want {
+				t.Errorf("buildURL() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCurrentMode(t *testing.T) {
+	type args struct {
+		mode string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "returns the current mode when in live mode",
+			args: args{"live"},
+			want: "live",
+		},
+		{
+			name: "returns the current mode when in test mode",
+			args: args{"test"},
+			want: "test",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			switch tt.args.mode {
+			case "live":
+				SwitchToLiveMode()
+			case "test":
+				SwitchToTestMode()
+			}
+
+			if got := CurrentMode(); got != tt.want {
+				t.Errorf("CurrentMode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
